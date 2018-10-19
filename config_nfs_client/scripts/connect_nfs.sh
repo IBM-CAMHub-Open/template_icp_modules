7257b5d5181cd3e0550d371a1fb209ebda94d0d5
CAMHub PublishBot <octravis@us.ibm.com>
2018-10-19 22:35:32 +0000

    Automated CAMHub::publish on 2018-10-19 22:35:32 UTC

--- config_nfs_client/scripts/connect_nfs.sh
+++ config_nfs_client/scripts/connect_nfs.sh
@@ -130,8 +130,8 @@ for ((i=0; i < ${NUM_FOLDERS}; i++)); do
     echo "${myfolderarray[i]} doesn't exist, creating..."
     mkdir -p ${myfolderarray[i]}
   fi
-
-  mount --bind ${myfolderarray[i]} $MOUNT_POINT/$last_folder
-  echo "${myfolderarray[i]} $MOUNT_POINT/$last_folder  none bind 0 0" >> /etc/fstab
+  ln -s $MOUNT_POINT/$last_folder ${myfolderarray[i]}
+  #mount --bind ${myfolderarray[i]} $MOUNT_POINT/$last_folder
+  #echo "${myfolderarray[i]} $MOUNT_POINT/$last_folder  none bind 0 0" >> /etc/fstab
 
 done
