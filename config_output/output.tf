d57f6960b6d73662008f75c606fd40b72c6bf1f5
CAMHub PublishBot <octravis@us.ibm.com>
2019-08-08 22:46:51 +0000

    Automated CAMHub::publish on 2019-08-08 22:46:51 UTC

--- config_output/output.tf
+++ config_output/output.tf
@@ -1,12 +1,15 @@
 output "cluster_kube_config"{
+  depends_on = ["camc_scriptpackage.get_cluster_config"]
   value = "${camc_scriptpackage.get_cluster_config.result["config"]}"
 } 
 
 output "cluster_kube_config_ca_cert_data"{
+  depends_on = ["camc_scriptpackage.get_cluster_config"]
   value = "${camc_scriptpackage.get_cluster_config.result["config_ca_cert_data"]}"
 } 
 
 output "registry_ca_cert"{
+  depends_on = ["camc_scriptpackage.get_cluster_config"]
   value = "${camc_scriptpackage.get_cluster_config.result["docker_cert"]}"
 } 
 
